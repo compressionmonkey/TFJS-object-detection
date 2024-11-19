7c57c35c807ca1d2f9b26a698306190b376686ba
Kelden Dradul Dorji <keldendraduldorji@gmail.com>
2024-11-19 16:24:45 +0600

    deploy test to vercel

---
+++ build/precache-manifest.70168f7a1c9d460b39b5a959a2201175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c547ec19dae2e07a37b5",
+    "url": "/static/css/main.b068b07a.chunk.css"
+  },
+  {
+    "revision": "c547ec19dae2e07a37b5",
+    "url": "/static/js/main.c547ec19.chunk.js"
+  },
+  {
+    "revision": "1faf93f60e54ee854402",
+    "url": "/static/js/1.1faf93f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "68fa9bc74f62551055a398d4373ac134",
+    "url": "/index.html"
+  }
+];
